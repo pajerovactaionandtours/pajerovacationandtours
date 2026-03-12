160ec46e4ec2cff80c65cc14cdc0e47b181e381d
pajerovactaionandtours <pajerogene2@gmail.com>
2026-03-12 07:38:05 +0530

    Update homejs.js

--- script.js/homejs.js
+++ script.js/homejs.js
@@ -1,20 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger");
-  const navLinks = document.querySelector(".nav-links");
-  const links = document.querySelectorAll(".nav-links a");
 
-  if (!hamburger || !navLinks) return;
-
-  hamburger.addEventListener("click", function () {
-    navLinks.classList.toggle("active");
-  });
-
-  links.forEach(link => {
-    link.addEventListener("click", function () {
-      navLinks.classList.remove("active");
-    });
-  });
-});
   /* =================How we work  ================= */
 
 const elements = document.querySelectorAll('.how-text, .how-image');
@@ -310,3 +294,4 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
