72659e6f6ad18829df46ebf03cba7fb68c2cd7bb
pajerovactaionandtours <pajerogene2@gmail.com>
2026-05-28 02:26:43 +0530

    Add files via upload

---
+++ script.js/faq.js
@@ -0,0 +1,12 @@
+document.querySelectorAll(".faq-question")
+.forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+const item=button.parentElement;
+
+item.classList.toggle("active");
+
+});
+
+});
